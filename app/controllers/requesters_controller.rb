eb56b84200c111a17d1b3f285d7fd7e1ad2e12c5
Jean Joeris <joeris.peter@gmail.com>
2016-10-18 15:26:49 -0600

    Refactor new user form and add requesters params

--- app/controllers/requesters_controller.rb
+++ app/controllers/requesters_controller.rb
@@ -4,5 +4,26 @@ class RequestersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to requesters_dashboard_path
+    else
+      render :new
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(
+        :first_name,
+        :last_name,
+        :email,
+        :phone,
+        :street_address,
+        :city,
+        :state,
+        :zipcode
+      )
+    end
 end
