3a30f92927993071e53b7ea3a110c9a6a2a74fe4
Jean Joeris <joeris.peter@gmail.com>
2016-10-18 15:05:05 -0600

    Change zip to zipcode on users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018204027) do
+ActiveRecord::Schema.define(version: 20161018205332) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20161018204027) do
     t.string   "street_address"
     t.string   "city"
     t.string   "state"
-    t.string   "zip"
+    t.string   "zipcode"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
