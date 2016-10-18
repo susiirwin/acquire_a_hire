7a1631598b0d102868b91153d2f221917a425c4d
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-10-18 16:00:58 -0600

    Add validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zipcode, presence: true
 
   has_many :user_roles
   has_many :roles, through: :user_roles
