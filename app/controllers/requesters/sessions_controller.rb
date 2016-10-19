79bac4a527243a553e85b348cecc8f4188aabc18
Jean Joeris <joeris.peter@gmail.com>
2016-10-18 20:03:12 -0600

    Add logout to view with helper, and corrisponding destroy action

--- app/controllers/requesters/sessions_controller.rb
+++ app/controllers/requesters/sessions_controller.rb
@@ -6,11 +6,18 @@ class Requesters::SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if is_valid_requester?(user)
       session[:user_id] = user.id
+      session[:current_role] = "requester"
       redirect_to requesters_dashboard_path
     else
       flash.now[:danger] = "Username and/or Password is invalid. Try again."
       render :new
     end
+  end
+
+  def destroy
+    session.clear
+    flash[:info] = "You have logged out"
+    redirect_to root_path
   end
 
   private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def logout_path_based_on_role
+    if session[:current_role] == 'professional'
+      professionals_logout_path
+    elsif session[:current_role] == 'requester'
+      requesters_logout_path
+    end
+  end
 end
