c899aa09c20fa84deec090148fc6d81b0ddae560
Jean Joeris <joeris.peter@gmail.com>
2016-10-18 20:03:50 -0600

    Extend logout functionality to professionals

--- app/controllers/professionals/sessions_controller.rb
+++ app/controllers/professionals/sessions_controller.rb
@@ -6,11 +6,18 @@ class Professionals::SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if is_valid_professional?(user)
       session[:user_id] = user.id
+      session[:current_role] = "professional"
       redirect_to professionals_dashboard_path
     else
       flash.now[:danger] = "Username and/or Password is invalid. Try again."
       render :new
     end
+  end
+
+  def destroy
+    session.clear
+    flash[:info] = "You have logged out"
+    redirect_to root_path
   end
 
   private
--- spec/features/user_logs_out_spec.rb
+++ spec/features/user_logs_out_spec.rb
@@ -1,19 +1,39 @@
 require 'rails_helper'
 
 describe "user log out" do
-  it "successfully logs out" do
+  it "successfully logs out a requester" do
     user = create(:user)
+    user.roles << Role.find_or_create_by(name: "requester")
 
-    visit login_path
+    visit requesters_login_path
 
-    fill_in "email", with: user.email
-    fill_in "password", with: user.password
+    fill_in "session_email", with: user.email
+    fill_in "session_password", with: user.password
 
-    click_on "log in"
+    click_on "Login"
 
-    click_on "Log out"
+    click_on "Logout"
 
-    expect(page).to have_content("log in")
+    expect(page).to have_content("Login")
+    expect(page).to_not have_content(user.first_name)
+    expect(page).to_not have_content(user.last_name)
+    expect(page).to_not have_content(user.street_address)
+  end
+
+  it "successfully logs out a professional" do
+    user = create(:user)
+    user.roles << Role.find_or_create_by(name: "professional")
+
+    visit professionals_login_path
+
+    fill_in "session_email", with: user.email
+    fill_in "session_password", with: user.password
+
+    click_on "Login"
+
+    click_on "Logout"
+
+    expect(page).to have_content("Login")
     expect(page).to_not have_content(user.first_name)
     expect(page).to_not have_content(user.last_name)
     expect(page).to_not have_content(user.street_address)
