e3bf98e97cb9dc1e79c29ca005ec261a7c449327
Jean Joeris <joeris.peter@gmail.com>
2016-10-18 20:09:10 -0600

    Refactor logging in to seperate method

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -5,12 +5,7 @@ describe "user log in" do
     user = create(:user)
     user.roles << Role.find_or_create_by(name: "requester")
 
-    visit requesters_login_path
-
-    fill_in "session_email", with: user.email
-    fill_in "session_password", with: user.password
-
-    click_on "Login"
+    login(user, requesters_login_path)
 
     expect(page).to have_content(user.first_name)
     expect(page).to have_content(user.last_name)
@@ -22,16 +17,20 @@ describe "user log in" do
     user = create(:user)
     user.roles << Role.find_or_create_by(name: "professional")
 
-    visit professionals_login_path
-
-    fill_in "session_email", with: user.email
-    fill_in "session_password", with: user.password
-
-    click_on "Login"
+    login(user, professionals_login_path)
 
     expect(page).to have_content(user.first_name)
     expect(page).to have_content(user.last_name)
     expect(page).to have_content(user.street_address)
     expect(current_path).to eq(professionals_dashboard_path)
+  end
+
+  def login(user, login_path)
+    visit login_path
+
+    fill_in "session_email", with: user.email
+    fill_in "session_password", with: user.password
+
+    click_on "Login"
   end
 end
