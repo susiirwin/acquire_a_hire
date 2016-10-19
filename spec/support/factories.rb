29e48628dd20167f7bea0bdf362e0020dc7ac176
Jean Joeris <joeris.peter@gmail.com>
2016-10-18 19:09:43 -0600

    Add user factory

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,3 +1,15 @@
 FactoryGirl.define do
   # factories for each model go here
+  factory :user do
+    first_name 'Chad'
+    last_name 'Clancey'
+    email 'cclancey007@test.com'
+    phone '555-555-1234'
+    street_address '123 Test St.'
+    city 'Denver'
+    state 'Colorado'
+    zipcode '80202'
+    password '12345'
+    password_confirmation '12345'
+  end
 end
