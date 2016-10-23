808ae5885af06c3ca3e9c77f72c3bba9e84cfbb5
Jean Joeris <joeris.peter@gmail.com>
2016-10-23 00:34:12 -0600

    Add validation on job so min price can't be greater than max

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,13 +1,21 @@
 class Job < ApplicationRecord
+  validate :is_valid_price_range
+
   belongs_to :skill
   belongs_to :requester, class_name: "User"
   belongs_to :professional, class_name: "User", optional: true
 
-  scope :in_progress, -> {where(status: "pending")}
-  scope :available, -> {where(status: "available")}
-  scope :closed, -> {where(status: "closed")}
+  scope :in_progress, -> { where(status: "pending") }
+  scope :available,   -> { where(status: "available") }
+  scope :closed,      -> { where(status: "closed") }
 
   def assigned_professional_name
     "none"
   end
+  private
+    def is_valid_price_range
+      if min_price > max_price
+        errors.add(:min_price, "can't be greater than max price")
+      end
+    end
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Job, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has "
+
+  it "requires min price less than max price" do
+    job = create(:job, min_price: 100, max_price: 200)
+    expect(job.valid?).to be true
+
+    job.min_price = 200
+    job.max_price = 100
+
+    expect(job.invalid?).to be true
+  end
 end
