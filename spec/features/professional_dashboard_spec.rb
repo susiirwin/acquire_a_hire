10559bfe2fe216322cfe59466ee873c573fe4cd5
Jean Joeris <joeris.peter@gmail.com>
2016-10-22 20:00:06 -0600

    Write test for pros seeing their jobs

--- spec/features/professional_dashboard_spec.rb
+++ spec/features/professional_dashboard_spec.rb
@@ -2,8 +2,17 @@ require 'rails_helper'
 
 describe "Professional dashboard" do
   context "current jobs" do
-    it "shows all open jobs assigned to me" do
-      
+    it "shows all in progress jobs assigned to me" do
+      pro = create(:professional_user)
+      job = create(:job, professional: pro, requester: create(:requester_user), status: "pending")
+      pro.jobs << job
+      ApplicationController.any_instance.stubs(:current_user).returns(pro)
+
+      visit professionals_dashboard_path
+
+      within('div.jobs') do
+        expect(page).to have_link("#{job.id} - #{job.title}")
+      end
     end
   end
 end
