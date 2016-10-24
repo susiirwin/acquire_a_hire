1a394b4bfe5f27ff1d100acf0955d628904fcd6a
Jean Joeris <joeris.peter@gmail.com>
2016-10-24 14:50:07 -0600

    Job creation now sets state

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :phone, presence: true
   validates :street_address, presence: true
   validates :city, presence: true
