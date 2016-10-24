836b35cdb4b091a921a6328dd65a0375bb41afa9
SusiIrwin <susi.irwin@gmail.com>
2016-10-24 08:00:48 -0600

    WIP - Form for New Requester Review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024025400) do
+ActiveRecord::Schema.define(version: 20161024135116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20161024025400) do
     t.integer  "requester_id",    null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "rating"
   end
 
   create_table "skills", force: :cascade do |t|
--- spec/features/professional_creates_review_for_requester_spec.rb
+++ spec/features/professional_creates_review_for_requester_spec.rb
@@ -4,25 +4,27 @@ describe "Professional leaves Review" do
   context "for requester" do
     it "shows review form and submitted review" do
       pro = create(:professional_user)
+      ApplicationController.any_instance.stubs(:current_user).returns(pro)
       job = create(:job, title: "Paint the Roof",professional: pro,
                     requester: create(:requester_user))
-      # when I visit a job assigned to me
+
       visit "/professionals/jobs/#{job.id}"
+
       expect(page).to have_content("Paint the Roof")
-      # and I click a "review requester" button
+
       click_on "Review Requester"
-      # i should be on a page for the requester/review/create
+
       expect(current_path).to eq(new_requesters_review_path)
-      # and I put in my review
-      fill_in 'requester_notes', with: 'This is my review of the requester'
-      # and I select a rating
-      choose('four_stars')
-      # and I click submit
-      click_on "Submit"
+
+      fill_in 'review_review', with: 'This is my review of the requester'
+      choose('review_rating_four_stars')
+      click_on "Create Review"
+
       # and I see the Review Show Page
       expect(current_path).to eq (requesters_review)
       # I expect the review text and the rating
       expect(page).to have_content("This is my review of the requester")
+      expect(page).to have_content("4 stars")
     end
   end
 end
