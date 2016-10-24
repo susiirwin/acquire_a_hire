485cb82fbd7d81c1732918859c70610726055cdf
Jean Joeris <joeris.peter@gmail.com>
2016-10-24 17:52:33 -0600

    Initial commit - add seeding of users in the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+  def self.start(num_requesters, num_professionals)
+    seed = Seed.new
+    seed.create_requester(num_requesters)
+    seed.create_professional(num_professionals)
+  end
+
+  def create_requester(num)
+    num.times do |i|
+      user = User.create(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: Faker::Internet.email,
+        phone: Faker::PhoneNumber.cell_phone,
+        street_address: Faker::Address.street_address,
+        city: Faker::Address.city,
+        zipcode: Faker::Address.zip_code,
+        password: Faker::Internet.password,
+        role: "requester",
+        state: Faker::Address.state_abbr,
+        verified: true
+      )
+      puts "Requester user #{i}: #{user.full_name} - #{user.email} created"
+    end
+  end
+
+  def create_professional(num)
+    num.times do |i|
+      user = User.create(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: Faker::Internet.email,
+        phone: Faker::PhoneNumber.cell_phone,
+        street_address: Faker::Address.street_address,
+        city: Faker::Address.city,
+        zipcode: Faker::Address.zip_code,
+        password: Faker::Internet.password,
+        role: "professional",
+        business_name: Faker::Company.name,
+        state: Faker::Address.state_abbr,
+        verified: true
+      )
+      puts "Professional user #{i}: #{user.business_name} - #{user.email} created"
+    end
+  end
+end
+
+Seed.start(100,100)
