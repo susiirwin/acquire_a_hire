4d0bed1fa9ecbdf883c6c6e9d6e5aa78efba1408
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-10-25 08:36:22 -0600

    Refactor message database call into model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,10 @@
 class MessagesController < ApplicationController
   def index
-    @messages = current_user.messages
-                            .where(job_id: params[:job])
-                            .where('sender_id = ? OR recipient_id = ?', params[:with], params[:with])
-                            .order(created_at: :desc)
+    @messages = current_user.messages.job_conversation(conversation_params)
   end
+
+  private
+    def conversation_params
+      params.permit(:job, :with)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -51,7 +51,7 @@ class User < ApplicationRecord
   end
 
   def messages
-    Message.where('sender_id = ? OR recipient_id= ?', self.id, self.id)
+    Message.where('sender_id = ? OR recipient_id = ?', self.id, self.id)
   end
 
   private
