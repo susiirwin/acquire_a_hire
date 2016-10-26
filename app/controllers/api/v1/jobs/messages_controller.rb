079657f179e5f2112c69204bbdf51a05191ded9d
Jean Joeris <joeris.peter@gmail.com>
2016-10-25 22:38:58 -0600

    Add subject to messaging api, clean up #create

--- app/controllers/api/v1/jobs/messages_controller.rb
+++ app/controllers/api/v1/jobs/messages_controller.rb
@@ -3,24 +3,29 @@ class Api::V1::Jobs::MessagesController < ApplicationController
 
   swagger_api :create do
     summary 'Send a message to a user'
-    notes 'Create a new message. Requires oauth token and id of user you are sending to'
+    notes 'Create a new message. Requires body, subject, recipient id and a valid token'
   end
 
   swagger_api :index do
     summary 'View all Messages in a Conversation'
-    notes 'Displays array of messages related to a job. Requires oauth token'
+    notes 'Displays array of messages related to a job. Requires a token'\
+          'If the requesting user is a professional, this will show the conversation they had with the job owner'\
+          'If the requesting user is a requester, this will show all the conversations with professionals about that job'
   end
 
   def create
     job = Job.find(params[:job_id])
     sender = UserAuthorization.find_by(token: params[:token]).user
     recipient = User.find(params[:recipient_id])
-    @name = if recipient.role == "professional"
-              recipient.business_name
-            else
-              recipient.full_name
-            end
-    Message.create!(body: params[:body], sender: sender, recipient: recipient, job: job)
+    @name = recipient_name(recipient)
+    Message.create!(
+      body: params[:body],
+      subject: params[:subject],
+      sender: sender,
+      recipient: recipient,
+      job: job
+    )
+
     render status: 201
   end
 
@@ -37,6 +42,14 @@ class Api::V1::Jobs::MessagesController < ApplicationController
 
   private
     def conversation_params
-      {job: params[:job_id], with: @requesting_user.id}
+      { job: params[:job_id], with: @requesting_user.id }
+    end
+
+    def recipient_name(recipient)
+      if recipient.role == "professional"
+        recipient.business_name
+      else
+        recipient.full_name
+      end
     end
 end
