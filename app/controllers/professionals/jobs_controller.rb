60480d0aae444b64f448d381fbdcef1c51cb886c
Jean Joeris <joeris.peter@gmail.com>
2016-10-25 18:53:16 -0600

    Refactor method for finding jobs in state with a pro's skills into job model

--- app/controllers/professionals/jobs_controller.rb
+++ app/controllers/professionals/jobs_controller.rb
@@ -1,7 +1,6 @@
 class Professionals::JobsController < ApplicationController
   def index
-    @jobs = Job.where(state: current_user.state)
-               .where(skill: [current_user.skills.pluck('id')])
+    @jobs = Job.for_professional(current_user)
   end
 
   def show
